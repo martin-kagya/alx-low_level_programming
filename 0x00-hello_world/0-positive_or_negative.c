5ea5d11fcf7d1e5b69b737e1607a22dbc208157d
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-16 05:09:52 -0700

    positive

--- 0x00-hello_world/0-positive_or_negative.c
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -11,16 +16,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n");
+		printf("%d is negative\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n")
+		printf("%d is zero\n", n);
 	}
-	/* your code goes there */
 	return (0);
 }
+
