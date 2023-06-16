3e53f113548eeb2a495e7228d7343e826e4e0451
martin-kagya <mkagyemang2@st.knust.edu.gh>
2023-06-16 03:26:02 +0000

    martin

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-It is where more programs are written
+ * return:means we have no return value for our main function
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art\n");
+    return (0);
+}
