f54037bfdc3541a5c57b511a899e0a6ec1df775a
martin-kagya <mkagyemang2@st.knust.edu.gh>
2023-06-16 02:14:46 +0000

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Starting point of the code 
+ *return: returns zero to show success of code
+ *
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+
+}    
