b5d3babb721e83913425a6570eec0ea4af18a1ad
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-15 21:25:27 -0700

    try

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main-It is where more programs are written
- * return:means we have no return value for our main function
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art");
-    return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
