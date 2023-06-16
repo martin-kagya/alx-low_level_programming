f029dd62c76ff5512706685723534423a272d7f3
martin-kagya <mkagyemang2@st.knust.edu.gh>
2023-06-16 04:14:03 +0000

    who

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main-It is the most important part
- * return: It must return 0 for succes
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
