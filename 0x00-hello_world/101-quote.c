36147291669b5b39a6ee5aa39cff6c6f68724be7
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-16 02:52:30 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,7 +8,10 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\"- Dora Korpar, 2015-10-19\n",  59);
+	const char quote[] = "and that piece of art is useful\n"
+		" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, quote, sizeof(quote));
+
 	return (1);
 }
