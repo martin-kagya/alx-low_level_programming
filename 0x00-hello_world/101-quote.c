1ddd561152ce0acca04e9800f80cde98988fda3f
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-16 04:00:58 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	const char quote[] = "and that piece of art is useful\""
-		"- Dora Korpar, 2015-10-19\n";
+		" - Dora Korpar, 2015-10-19\n";
 
 	write(STDERR_FILENO, quote, sizeof(quote));
 
