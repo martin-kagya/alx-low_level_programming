bb75b64f04b87dc2b510557fde7eb5e954887e13
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-16 06:56:15 -0700

    martin

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char letter;
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-            putchar(letter);
-    }
-    putchar('\n');
-    return (0);
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		putchar(letters);
+	}
+	putchar('\n');
+	return (0);
 }
