d457e473fac228ae522ac9397b4dabe1aa1eb63d
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-19 02:54:48 -0700

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = abs(n) % 10;
+	last = n % 10;
+
+	if(n < 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	}
 
 	if (last > 5)
 	{
