c3772fd9096f4f6fdc4bbbee5e277fc8078962eb
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-19 03:32:17 -0700

    martin

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int i;
 
-	char numberz[] = "0123456789";
+	int numberz[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 	for (i = 0; i < 10; i++)
 	{
-		char out = numberz[i];
+		int out = numberz[i];
 
-		putchar(out);
+		putchar((char)out + '0');
 	}
 	putchar('\n');
 	return (0);
