b5cf66f503931a4f73598a4db5d8c97a4b9f711c
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-19 04:55:47 -0700

    print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,16 @@
  */
 int main(void)
 {
-	char number[] = "0123456789";
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(number[i]);
-		putchar(',');
-		putchar(' ');
+		putchar((char)i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
