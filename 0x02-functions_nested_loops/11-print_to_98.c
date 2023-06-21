1a10c45e4294022ef1c192fcba2391583f11d44b
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-21 08:52:21 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints natural numbers to 98
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		_putchar(98);
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int _abs(int c);
 int print_last_digit(int l);
 void times_table(void);
 int add(int a, int b);
+void print_to_98(int n);
