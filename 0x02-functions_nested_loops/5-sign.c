42be9bb34a6829c8acde53d2ad26ee015e152d5d
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-20 20:20:08 -0700

    sign checks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Checks the sign of numbers
+ * @c: the number to be checked
+ * Return: 1 when positive -1 when negative 0 for zeros
+ */
+int print_sign(int c)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
