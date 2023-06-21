4717e9e8477f122e48aeee63b0a5b6e6206a4b79
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-21 11:55:13 -0700

    100-times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * print_times_table - Prints the times table of integer n
+ * Return: Always 0 (Success)
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n >= 0 && n <= 14)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (c > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar((c / 10) % 10 + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if (c > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c / 10) % 10 + '0');
+					_putchar((c % 10) + '0');
+				}
+				else
+				{
+					if (b != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n')
+		}
+	}
+}
