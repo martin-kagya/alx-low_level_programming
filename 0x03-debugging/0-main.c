d9f9e7f2d70cd2afdf0202e3a693fdeddb568325
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-21 15:07:48 -0700

    betty

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
-#include "main.h"
+i#include "main.h"
 /**
  * main - it runs a test for positive and negative function
+ * positive_or_negative - it tests if a number is positive or negative
  * Return: Always 0 (Success)
  */
 int main(void)
