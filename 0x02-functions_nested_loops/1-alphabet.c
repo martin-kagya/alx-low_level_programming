c1b609c4c30bf4f5021dc086dfbdb5d8a657352a
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-20 19:37:02 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * main - Lists alphabets
- * Reurn: Always 0 (SUccess)
+ * print_alphabet - Lists alphabets
+ * Return: Always 0 (SUccess)
  */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c < 'z'; c++)
 	{
 		_putchar(c);
