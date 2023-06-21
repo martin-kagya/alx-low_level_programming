c3db7546990118d93537cc62d7608a041909215e
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-21 15:03:38 -0700

    test for sign

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - it runs a test for positive and negative function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int test;
+
+	test = 0;
+	positive_or_negative(test);
+	return (0);
+}
