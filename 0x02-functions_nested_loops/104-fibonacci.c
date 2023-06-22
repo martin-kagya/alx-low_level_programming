0e5689ff73a7257413536e6878dc8b6df12c87a7
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-21 17:28:44 -0700

    98-fub

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio>
+/**
+ * main - Prints fibonacci numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int a, b, c;
+	int i;
+
+	a = 0;
+	b = 1;
+	for (i = 1; i <= 98; i++)
+	{
+		if (i != 98)
+		{
+		c = a;
+		a = b;
+		b = c + b;
+		printf("%lu, ", b);
+		}
+		else
+		{
+			c = a;
+			a = b;
+			b = c + b;
+			printf("%lu", b);
+		}
+	}
+	printf("\n");
+	return (0);
+}
