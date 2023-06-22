c3825b63ba5b2b7e2f9ec0042e4bfc10491226f4
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-22 13:35:34 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,23 +9,29 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i != 100)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf("Buzz ");
+			if (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (i % 3 == 0 && i % 5 != 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i % 5 == 0 && i % 3 != 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%d ", i);
+			}
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 	}
-	printf("\n");
 	return (0);
 }
