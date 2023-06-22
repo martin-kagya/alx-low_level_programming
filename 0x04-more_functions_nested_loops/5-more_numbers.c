9bb5d094674f88ba278bab64fe9a70e2f5636050
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-22 09:05:07 -0700

    more numerals

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print fourteen digits ten times
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
