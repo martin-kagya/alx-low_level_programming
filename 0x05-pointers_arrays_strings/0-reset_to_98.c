bbaa1ed307bbbfd22f48baa0ce11307a66b2c5d3
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-26 09:22:51 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes a number to 98
+ * @n: a pointer to be changed
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This repositoty is about learning more aboit pointers and
+arrays in c programming
