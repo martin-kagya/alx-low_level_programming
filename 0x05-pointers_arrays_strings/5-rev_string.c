186db177200be45ea9920718292d58091c862c97
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-26 12:44:43 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverses the string
+ * @s - string to be reversed
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int len, len1;
+
+	len = 0;
+	len1 = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+	for (; len1 >= len / 2; len1--)
+	{
+		temp = s[len1];
+		s[len1] = s[len - len1 - 1];
+		s[len - len1 - 1] = temp;
+	}
+}
