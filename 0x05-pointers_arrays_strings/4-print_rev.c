c1df9384d9291bb3503e3224d93caa9d7594ba2a
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-26 11:22:16 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints strings in reverse
+ * @s: string to be reversed
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++
+	}
+	for (; length >= 0; length--)
+	{
+		_putchar(str[length]);
+	}
+	_putchar('\n');
+}
