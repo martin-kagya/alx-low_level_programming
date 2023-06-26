afe2f941b361479999661811912762d19fd4e115
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-26 13:16:31 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints half of the string given
+ * @str: string for operation
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	int length, i;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < length - 1; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
