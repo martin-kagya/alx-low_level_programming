abf0b7df73beef7d652da9bfe0376abf1bbd5d7f
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-26 10:41:29 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - functions as the c put function
+ * @str: string for operation
+ */
+
+void _puts(char *str)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		_putchar(s[length]);
+		length++;
+	}
+}
