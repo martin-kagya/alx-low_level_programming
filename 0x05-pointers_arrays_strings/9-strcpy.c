aacb7812e82dd5255f905f65d4566c0ee91511f5
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-26 18:31:31 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string
+ * @dest: where to copy to
+ * @src: string to be copied
+ * Return: returns the copied string in dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i != src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
