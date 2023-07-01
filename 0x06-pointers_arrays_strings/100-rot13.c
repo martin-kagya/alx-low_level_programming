de15b4c7a0dfe9d0bd7aa33129e07757c7f57351
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-06-30 19:47:56 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 -  a function that encodes a string using rot13.
+ * @str: An input string to encode using rot13
+ * Return: An encoded string
+ */
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		while ((str[i] >= 'a' && str[i] <= 'z') ||
+				(str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			if ((str[i] >= 'a' && str[i] <= 'm') ||
+					(str[i] >= 'A' && str[i] <= 'M'))
+				str[i] += 13;
+			else
+				str[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	return (str);
+}
