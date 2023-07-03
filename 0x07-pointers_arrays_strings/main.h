bdf2f82b1e8ab1e177d1ff3a94d3b996bd930427
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-07-03 13:44:15 -0700

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,2 @@
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *str);
-char *cap_string(char *str);
-char *leet(char *s);
-void print_number(int n);
- char *rot13(char *str);
+char *_memset(char *s, char b, unsigned int n);
