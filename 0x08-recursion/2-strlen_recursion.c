d4fdf4044db079200fc58c2852f8c4dbd51ef6ab
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-07-05 19:37:49 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - gives the string length
+ * @s: string to count on
+ * Return: returns the string length
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		count = _strlen_recursion(s + 1);
+		return (count + 1);
+	}
+}
