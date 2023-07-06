d466489b71c51791801eb89846e7b60524c450d5
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-07-05 19:24:27 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to be reversed
+ * Return: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_putchar(s + 1);
+		_putchar(*s);
+		_putchar('\n');
+	}
+}
