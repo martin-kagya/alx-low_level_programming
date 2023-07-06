146e08b5f56191ea7fbe1762a6f39340d88c722e
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-07-05 19:01:18 -0700

    recursion string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - functions like the puts function
+ * @s: string to be printed
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
