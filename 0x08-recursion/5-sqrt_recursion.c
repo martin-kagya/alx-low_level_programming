9c9527b0adca2721ee491566e0fc7e181da03d32
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-07-06 19:24:40 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * check - checks number from n to base
+ * @n: the startin point of check
+ * @b: the end point of base
+ * Return: n as the sqrt when found -1 for failed attempt
+ * and a recursive function
+ */
+int check(n, b)
+{
+	if (n *n == b)
+	{
+		return (n);
+	}
+	if (n *n > b)
+	{
+		return (-1);
+	}
+	return (check(n + 1, b));
+}
+/**
+ * _sqrt_recursion - returns the square root of a natural number
+ * @n: the base number
+ * Return: the recursive function check
+ */
+int _sqrt_recursion(int n)
+{
+	return (check(1, n));
+}
