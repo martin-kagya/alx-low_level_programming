9f5504f67a946cba936b5a1d162a8c783b7d733b
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-07-09 06:45:24 -0700

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point of code
+ * @argc: argument count
+ * @argv[]: argument vector
+ */
+int main(argc, char* argv[])
+{
+	(void) argc;
+	printf("%s", argv[0]);
+	return 0;
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+s is the alx argc and argv trainging program
