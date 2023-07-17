977abbd6a41448c23d49d63d2144bcd704927b0c
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-07-17 15:15:34 -0700

    poopy

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib>
+#include "dog.h"
+/**
+ * init_dog - functions to initialize the struct
+ * @d: variable for the struct
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner's name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
