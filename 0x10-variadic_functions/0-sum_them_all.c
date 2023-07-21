75ff4c7b8f859137d4dbcc23be0f4b15c419a13f
Martin Kagya Agyemang <mkagyaagyemang777@gmail.com>
2023-07-21 14:59:41 -0700

    suming

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,12 +3,17 @@
 /**
  * sum_them_all - function to sum numbers in parameters
  * @n: number of arguments to be passed
+ * Return: 0 when n is zero and sum upon success
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	int i, sum = 0;
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
