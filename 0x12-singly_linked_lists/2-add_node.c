99bca02bcacbdedce3581fcccb1420264775cd32
martin-kagya <martinkagya777@gmail.com>
2023-09-22 19:35:41 -0700

    list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - add node to to the beginning of the list
+ * @head: head for the list
+ * @str: string to be copied
+ * Return: returns 0;
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		return (NULL);
+		free(new);
+	}
+	new->next = *head;
+	*head = new->next;
+}
