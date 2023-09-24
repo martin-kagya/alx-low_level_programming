d04b53bbf950a19c50140d0fd84bb0a26e1035c9
martin-kagya <martinkagya777@gmail.com>
2023-09-23 17:58:01 -0700

    industry

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * first - function that executes before main
  * Return: Always 0
  */
-void first()
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
