2a6540c6599a1ed0014b6eee30af4a5c4621d89f
martin-kagya <martinkagya777@gmail.com>
2023-09-25 18:52:30 -0700

    get index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - get the node at a specific index
+ * @head: pointer to the head of list
+ * @index: index to return itds node
+ * Return: node at index 
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node = head;
+	unsigned int i;
+
+	for (i = 0; i < index; i++)
+	{
+		if (node == NULL)
+		{
+			return (NULL);
+		}
+		node = node->next;
+	}
+	return (node);
+}
