5882c0c6ec4e1fbcb8c5758c79e06740ba3397e7
martin-kagya <martinkagya777@gmail.com>
2023-09-26 16:36:35 -0700

    not sure

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints the list
+ * @head: pointer to the head of list
+ * Return: returns number of elements or 98 on error
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i = 0;
+	const listint_t *current = head;
+
+	while (current != NULL)
+	{
+		if (current == head)
+		{
+			exit(98);
+		}
+		printf("%d\n", current->n);
+		i++;
+		current = current->next;
+	}
+	return (i);
+}
